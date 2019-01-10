a20ca39ad599f23641a4ef0d61e9b167d8836fc4
Nick Inman <nick@theoatmeal.com>
2019-01-10 20:49:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,10 @@ class Student
   def initialize(name, grade)
     @name = name
     @grade = grade
+  end
+
+  def self.create_table
+
   end 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
