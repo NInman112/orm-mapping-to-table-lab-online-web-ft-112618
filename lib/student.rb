e9c3003856785fc8d4710964785160c7188c1f4d
Nick Inman <nick@theoatmeal.com>
2019-01-10 20:47:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,10 @@ class Student
   attr_reader :id
 
   def initialize(name, grade)
-
+    @name = name
+    @grade = grade
   end 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
 end
